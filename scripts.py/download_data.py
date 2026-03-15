5dfd5548ef068724e2dde2eba9ef86cbe1991401
Chukwunwike Mark Anwuluorah <markchinwike@gmail.com>
2026-03-15 22:36:16 +0100

    Day 2 & 3: Taxonomic fine-tuning, Roboflow integration, and error analysis

--- scripts.py/download_data.py
+++ scripts.py/download_data.py
@@ -6,4 +6,3 @@ project = rf.workspace("marks-workspace-chqhs").project("pollinator_monitor_vb")
 version = project.version(2)
 dataset = version.download("yolov8")
 
-yolo task=detect mode=train model=yolov8n.pt data=Pollinator_Monitor_vb-2/data.yaml epochs=25 imgsz=640 plots=True
